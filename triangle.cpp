d3e362bc3f18233bcc28a97c51cefaf0f41c524f
say-no-to-sleep <eugenezhao63@gmail.com>
2024-10-11 19:19:30 -0400

    Added files

---
+++ triangle.cpp
@@ -0,0 +1,8 @@
+// Assumption: The triangle is right-angled with a height of 3 and a base of 4.
+// It is displayed as:
+// ****
+// **
+// *
+
+#include<iostream>
+int main(){std::cout<<"****\n**\n*\n";}
